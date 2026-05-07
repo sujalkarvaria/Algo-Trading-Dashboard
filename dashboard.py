d3932206962a1a00dc5ffd669a103765eb4b4459
sujalkarvaria <sujalkarwariya047@gmail.com>
2026-05-07 22:49:37 +0530

    Fixed close values error

--- dashboard.py
+++ dashboard.py
@@ -80,8 +80,9 @@ else:
     current_signal = "HOLD"
 
 # ---------------- Live Price Box ----------------
-last_price = pd.to_numeric(df["Close"], errors="coerce").iloc[-1]
-prev_price = pd.to_numeric(df["Close"], errors="coerce").iloc[-2]
+
+last_price = float(df["Close"].values[-1])
+prev_price = float(df["Close"].values[-2])
 
 last_price = float(last_price)
 prev_price = float(prev_price)
